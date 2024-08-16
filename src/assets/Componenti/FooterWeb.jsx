1338e478931f460e41c41109b0352986f5e6fb3d
FraConv <francesco16convertini@gmail.com>
2024-08-16 12:45:31 +0200

    modifiche 8

--- src/assets/Componenti/FooterWeb.jsx
+++ src/assets/Componenti/FooterWeb.jsx
@@ -4,10 +4,15 @@ function FooterWeb(){
 
         <>
 
-            <footer className=" w-[100%] h-24  left-0 absolute bg-[#443e3e]"><h6 className=" text-white text-[12px] mt-[40px] ml-[-60px] min-[1400px]:ml-[-1360px] 
-                min-[1400px]:text-[15px]
-                xl:ml-[-860px]  min-[768px]:text-[15px] min-[768px]:ml-[-260px]">
+            <footer className=" w-[100%] h-24  left-0 absolute bg-[#443e3e]">
+                <h6 className=" text-white text-[12px] mt-[40px] ml-[-60px] min-[1400px]:ml-[-1360px] min-[1400px]:text-[15px] xl:ml-[-860px]  
+                min-[768px]:text-[15px] min-[768px]:ml-[-260px]">
                 Copyright © 2023 FrancescoConvertini Portfolio</h6>
+                <a className=" text-white hover:text-[#f9b65d] text-[12px] ml-[-175px] min-[1400px]:text-[15px] pt-[60px] md:ml-[-445px] xl:ml-[-1045px] 
+                min-[1400px]:ml-[-1492px]" 
+                href="https://www.iubenda.com/privacy-policy/13617581">Privacy Policy</a>
+                <a className=" text-white hover:text-[#f9b65d] text-[12px] min-[1400px]:text-[15px] ml-[-170px] pt-[60px]  min-[1400px]:ml-[-226px]" 
+                href="https://www.iubenda.com/privacy-policy/13617581/cookie-policy">Cookie Policy</a>
             </footer>
 
         </>
